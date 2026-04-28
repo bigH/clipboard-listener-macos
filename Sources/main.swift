da88b42f773888a79abc02f7054622de5fa95c0c
Hiren Hiranandani <hiren.hiranandani@chime.com>
2026-04-28 15:02:15 -0700

    support exit-on-first-copy

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,33 +1,53 @@
 import Cocoa
 
-class PasteboardWatcher : NSObject {
-    private let pasteboard = NSPasteboard.general
-    private var changeCount : Int
-    private var timer: Timer?
+struct Options {
+    let exitOnFirstCopy: Bool
 
-    override init() {
-        changeCount = pasteboard.changeCount
-        super.init()
-    }
-
-    func startPolling () {
-        timer = Timer.scheduledTimer(
-            withTimeInterval: 0.05,
-            repeats: true,
-            block: { (t: Timer) -> Void in
-                if self.pasteboard.changeCount != self.changeCount {
-                    if let copiedString = self.pasteboard.string(forType: .string) {
-                        print(copiedString)
-                        self.changeCount = self.pasteboard.changeCount
-                    }
-                }
-            }
-        )
+    static func parse(arguments: [String]) -> Options {
+        Options(exitOnFirstCopy: arguments.dropFirst().contains("--exit-on-first-copy"))
     }
 }
 
-let watcher = PasteboardWatcher()
+final class PasteboardWatcher: NSObject {
+    private let pasteboard = NSPasteboard.general
+    private let exitOnFirstCopy: Bool
+    private var changeCount: Int
+    private var timer: Timer?
+
+    init(exitOnFirstCopy: Bool) {
+        self.exitOnFirstCopy = exitOnFirstCopy
+        self.changeCount = pasteboard.changeCount
+        super.init()
+    }
+
+    func startPolling() {
+        timer = Timer.scheduledTimer(
+            withTimeInterval: 0.05,
+            repeats: true,
+            block: { [weak self] (_: Timer) -> Void in
+                self?.handlePasteboardChange()
+            }
+        )
+    }
+
+    private func handlePasteboardChange() {
+        let currentChangeCount = pasteboard.changeCount
+        guard currentChangeCount != changeCount else { return }
+
+        changeCount = currentChangeCount
+
+        guard let copiedString = pasteboard.string(forType: .string) else { return }
+
+        print(copiedString)
+
+        if exitOnFirstCopy {
+            Foundation.exit(EXIT_SUCCESS)
+        }
+    }
+}
+
+let options = Options.parse(arguments: CommandLine.arguments)
+let watcher = PasteboardWatcher(exitOnFirstCopy: options.exitOnFirstCopy)
 watcher.startPolling()
 
 RunLoop.main.run()
-
